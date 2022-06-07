c139efcf1b4faa01d8473583d8715b72f7daecd7
moshiulrabbi <mhrabby61@gmail.com>
2022-06-07 22:43:48 +0600

    rm_link

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import "font-awesome/css/font-awesome.min.css";
 import "./index.css";
 import React from "react";
 
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-/>;
-
-<link
-  href="https://fonts.googleapis.com/css?family=PT+Serif:400,700|Lato:300,400,700&display=swap"
-  rel="stylesheet"
-/>;
-
 class App extends React.Component {
   constructor(props) {
     super(props);
